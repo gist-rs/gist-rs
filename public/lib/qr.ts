09f5601affc2ca11df8cd4ed6fd50f4ea11396a4
katopz <katopz@gmail.com>
2022-12-10 00:11:12 +0700

    adjust qr scale

--- public/lib/qr.ts
+++ public/lib/qr.ts
@@ -1,3 +1,3 @@
 export const get_qr = (url: string, size = 256) => {
-  return `https://chart.googleapis.com/chart?cht=qr&chl=${url}&chs=${size}x${size}`
+  return `https://chart.googleapis.com/chart?cht=qr&chld=M|2&chl=${url}&chs=${size}x${size}`
 }
