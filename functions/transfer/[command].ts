f5df95b33794826c970786f7bbfd68778ee6e4d4
katopz <katopz@gmail.com>
2022-12-10 23:51:11 +0700

    poc: transfer

--- functions/transfer/[command].ts
+++ functions/transfer/[command].ts
@@ -3,8 +3,15 @@ interface Env {
   SERVICE: Fetcher;
 }
 
-export const onRequest: PagesFunction<Env> = async (context) => {
-  const { command } = context.params
-  console.log('command:', command)
-  return context.env.SERVICE.fetch(context.request);
+// export const onRequest: PagesFunction<Env> = async (context) => {
+//   const { command } = context.params
+//   console.log('command:', command)
+//   if (!context.env.SERVICE) {
+//     return new Response('❌ expect SERVICE')
+//   }
+//   return context.env.SERVICE.fetch(context.request);
+// }
+
+export function onRequest(context) {
+  return new Response(JSON.stringify(context.params.user) + ':' + !!context.env.SERVICE)
 }
