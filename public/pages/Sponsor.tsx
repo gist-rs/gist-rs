4ad6ffb8fff30c306826eeb81054e78da77cb8f8
katopz <katopz@gmail.com>
2022-12-10 10:01:03 +0700

    add is mobile

--- public/pages/Sponsor.tsx
+++ public/pages/Sponsor.tsx
@@ -2,16 +2,36 @@ import { useEffect, useState } from 'preact/hooks'
 import { get_qr } from '../lib/qr'
 import { get_solana_pay_link, Web3LiteClient, Web3LiteWallet } from '../lib/web3lite'
 
+import mobile from 'is-mobile'
+const is_mobile = mobile()
+
 export const GIST_PUBKEY = 'gistmeAhMG7AcKSPCHis8JikGmKT9tRRyZpyMLNNULq'
 export const PATREON_URL = 'https://patreon.com/gist_rs'
 const web3l_client = new Web3LiteClient()
 
+const SolanaPay = () => {
+  const default_solana_pay_link = get_solana_pay_link(GIST_PUBKEY)
+
+  if (!is_mobile) return <></>
+
+  return (
+    <>
+      <img src={get_qr(default_solana_pay_link)} />
+      <hr />
+      <a href={default_solana_pay_link} title="Solana Pay" aria-label="Solana Pay">
+        Solana Pay {default_solana_pay_link}
+      </a>
+
+      <hr />
+      <a href="https://phantom.app/ul/v1/connect?app_url=https://gist.rs&dapp_encryption_public_key=Bigf1Q8vseNX5TAGZpKkuoC51HMMmhZZCWrVBwy7R8kP&redirect_link=https://gist.rs/auth/phantom&">connect</a>
+      <hr />
+    </>
+  )
+}
+
 export default function Sponsor() {
   const [public_key, set_public_key] = useState<string>()
   const [lamports, set_lamports] = useState(0)
-
-  const default_solana_pay_link = get_solana_pay_link(GIST_PUBKEY)
-  const [solana_pay_link] = useState<string>(default_solana_pay_link)
 
   useEffect(() => {
     web3l_client
@@ -39,14 +59,9 @@ export default function Sponsor() {
       <pre class="tag">◎ {lamports}</pre>
       <pre class="tag">{public_key}</pre>
       <hr />
-      <img src={get_qr(solana_pay_link)} />
-      <hr />
-      <a href={solana_pay_link} title="Solana Pay" aria-label="Solana Pay">
-        Solana Pay {solana_pay_link}
-      </a>
-      <hr />
-      <a href="https://phantom.app/ul/v1/connect?app_url=https://gist.rs&dapp_encryption_public_key=Bigf1Q8vseNX5TAGZpKkuoC51HMMmhZZCWrVBwy7R8kP&redirect_link=https://gist.rs/auth/phantom&">connect</a>
-      <hr />
+
+      <SolanaPay />
+
       <img src={get_qr(PATREON_URL)} alt={PATREON_URL} />
       <hr />
       <a href={PATREON_URL} title="Sponsor" aria-label="Patreon" target="_blank" rel="noopener">
