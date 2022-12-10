259e8da8f849a63a43c1b8206359e7609ed917fb
katopz <katopz@gmail.com>
2022-12-10 22:39:58 +0700

    working cookie

--- functions/auth/[provider].ts
+++ functions/auth/[provider].ts
@@ -1,7 +1,9 @@
 
 import * as bs58 from "bs58";
 import * as nacl from "tweetnacl";
+import * as cookie from "cookie";
 
+const COOKIES_DOMAIN = '.gist.rs'
 export enum ProviderName {
   Phantom = "phantom"
 }
@@ -17,8 +19,22 @@ const decryptPayload = (data: string, nonce: string, sharedSecret: Uint8Array) =
   return JSON.parse(payload);
 };
 
-const handle_phantom_deeplink = async (context, searchParams: URLSearchParams) => {
-  // console.log('context:', context)
+const get_cookie_web3_token = (web3_token: string, cookies_domain: string) => {
+  return cookie.serialize('web3_token', web3_token, {
+    domain: cookies_domain,
+    httpOnly: true,
+    secure: true,
+    sameSite: cookies_domain !== '' ? 'lax' : 'none',
+    path: '/',
+    maxAge: 60 * 60 * 1 // 1 hour
+  })
+}
+
+const handle_phantom_deeplink = async (context) => {
+  console.log('context:', context)
+  const request: Request = context.request;
+  const { searchParams, hostname } = new URL(request.url)
+
   const PHANTOM_ENCRYPTION_SECRET_KEY = await context.env.PHANTOM_ENCRYPTION_SECRET_KEY
   const secret_key = new Uint8Array(bs58.decode(PHANTOM_ENCRYPTION_SECRET_KEY));
   const sharedSecretDapp = nacl.box.before(
@@ -32,16 +48,19 @@ const handle_phantom_deeplink = async (context, searchParams: URLSearchParams) =
     sharedSecretDapp
   );
 
-  return new Response(`Welcome:${JSON.stringify(connectData, null, 2)}`)
+  const response = new Response(`Welcome:${JSON.stringify(connectData, null, 2)}`)
+  const web3_token = `${connectData.public_key},${connectData.session}`
+  const cookies_domain = ['localhost'].includes(hostname) ? '' : COOKIES_DOMAIN
+  const cookie_web3_token = get_cookie_web3_token(web3_token, cookies_domain)
+  response.headers.set('Set-Cookie', cookie_web3_token)
+
+  return response
 }
 
 export function onRequest(context) {
-  const request: Request = context.request;
-  const { searchParams } = new URL(request.url)
   const { provider } = context.params
-
   switch (provider) {
-    case ProviderName.Phantom: return handle_phantom_deeplink(context, searchParams)
+    case ProviderName.Phantom: return handle_phantom_deeplink(context)
     default: return new Response('Wat?')
   }
 }
