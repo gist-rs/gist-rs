99d34a77f629428497bc6bca91f814d32cbb16a5
katopz <katopz@gmail.com>
2022-12-12 17:24:09 +0700

    use session data or mocked

--- public/pages/Auth.tsx
+++ public/pages/Auth.tsx
@@ -1,45 +1,20 @@
-// import { ErrorBoundary, Router, LocationProvider, hydrate, prerender as ssr, lazy } from 'preact-iso'
-// import { useEffect, useState } from 'preact/hooks'
-// import { render } from 'preact'
-
-// export function AuthRoot() {
-//   return (
-//     <LocationProvider>
-//       <ErrorBoundary>
-//         {/* <Router>
-//           <Auth path="/auth/:provider">Hello</Auth>
-//           <></>
-//         </Router> */}
-//         Hello!!
-//         <>{}</>
-//       </ErrorBoundary>
-//     </LocationProvider>
-//   )
-// }
-
-// hydrate(<AuthRoot />)
-
-// export async function prerender(data) {
-//   console.log('data:', data)
-//   return await ssr(<AuthRoot {...data} />)
-// }
-
-// export default function Auth() {
-//   return <div>Hellooooo</div>
-// }
-
 import { render } from 'preact'
-import { useEffect, useState } from 'preact/hooks'
+import { useState } from 'preact/hooks'
 import { get_user_data } from '../lib/cf'
 
 const Auth = () => {
-  const [pubkey] = useState(get_user_data().server_session.pubkey)
+  const user_data = get_user_data()
+  const [pubkey] = useState(user_data.pubkey)
+  const [session] = useState(user_data.session)
   return (
     <div>
       <pre>
         <code>{pubkey}</code>
       </pre>
-      <p>Do you agree to the statement: "Preact is awesome"?</p>
+      <pre>
+        <code>{session}</code>
+      </pre>
+      <a href={`/u/${pubkey}`}>continue</a>
     </div>
   )
 }
