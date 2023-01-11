84ffe13fbbb1bb645a5cb2cfb8504a59f208d524
katopz <katopz@gmail.com>
2023-01-11 14:55:13 +0700

    fix: remove comment

--- functions/nft/[address].ts
+++ functions/nft/[address].ts
@@ -1,4 +1,3 @@
 export function onRequest(context) {
-  // return new Response(`TODO: Read cached from KV: ${context.params.address}`);
   return context.env.SERVICE.fetch(context.request, context);
 }
--- functions/transfer/[command].ts
+++ functions/transfer/[command].ts
@@ -1,18 +1,3 @@
-
-// interface Env {
-//   SERVICE: Fetcher;
-// }
-
-// export const onRequest: PagesFunction<Env> = async (context) => {
-//   const { command } = context.params
-//   console.log('command:', command)
-//   if (!context.env.SERVICE) {
-//     return new Response('❌ expect SERVICE')
-//   }
-//   return context.env.SERVICE.fetch(context.request);
-// }
-
 export function onRequest(context) {
-  // return new Response(JSON.stringify(context.params.command) + ':' + !!context.env.SERVICE)
   return context.env.SERVICE.fetch(context.request, context);
 }
