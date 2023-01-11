080d31bb5ec166a7d65573d1b37049598f7d0d65
katopz <katopz@gmail.com>
2023-01-11 10:51:45 +0700

    wip: nft

--- functions/nft/[address].ts
+++ functions/nft/[address].ts
@@ -1,4 +1,4 @@
 export function onRequest(context) {
-  return new Response(`TODO: Read cached from KV: ${context.params.address}`);
-  // return context.env.SERVICE.fetch(context.request, context);
+  // return new Response(`TODO: Read cached from KV: ${context.params.address}`);
+  return context.env.SERVICE.fetch(context.request, context);
 }
