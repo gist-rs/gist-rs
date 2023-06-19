f29ea5e091c6031d3d71a365d870dc23ace7035b
katopz <katopz@gmail.com>
2023-06-19 09:32:55 +0700

    fix: _ctx.param("url")

--- worker/web3lite/src/handlers/proxy.rs
+++ worker/web3lite/src/handlers/proxy.rs
@@ -5,11 +5,16 @@ pub async fn fetch_json(url: &str) -> anyhow::Result<serde_json::Value> {
 }
 
 pub async fn handle_proxy_req(_req: Request, _ctx: RouteContext<()>) -> Result<Response> {
-    let url = _ctx.params.url;// "https://raw.githubusercontent.com/gist-rs/book/main/examples/r4/20-fetch-json-reqwest/src/foo.json";
-    let json_result = fetch_json(url).await;
+    let url = _ctx.param("url"); // "https://raw.githubusercontent.com/gist-rs/book/main/examples/r4/20-fetch-json-reqwest/src/foo.json";
+    match url {
+        Some(url) => {
+            let json_result = fetch_json(url).await;
 
-    match json_result {
-        Ok(value) => Response::ok(value.to_string()),
-        Err(err) => Response::error(format!("${err}"), 500),
+            match json_result {
+                Ok(value) => Response::ok(value.to_string()),
+                Err(err) => Response::error(format!("${err}"), 500),
+            }
+        }
+        None => Response::error("Expected url params".to_string(), 404),
     }
 }
