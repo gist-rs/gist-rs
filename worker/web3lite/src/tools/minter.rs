37ec1dfa082880d57931e9b8d64ab75d60e00100
katopz <katopz@gmail.com>
2023-06-24 14:31:16 +0700

    wip: token22

--- worker/web3lite/src/tools/minter.rs
+++ worker/web3lite/src/tools/minter.rs
@@ -1,179 +1,166 @@
+use std::convert::TryInto;
+
 use solana_web3_wasm::{
     solana_client_wasm::WasmClient,
     solana_extra_wasm::program::{
         spl_associated_token_account::{self, instruction::create_associated_token_account},
         spl_token,
+        spl_token_2022::extension::transfer_fee::{TransferFee, TransferFeeConfig},
     },
-    solana_sdk::{pubkey::Pubkey, transaction::Transaction},
+    solana_sdk::{
+        program_option::COption, pubkey::Pubkey, signature::Keypair, signer::Signer,
+        transaction::Transaction,
+    },
 };
 
-struct MintToInfo {
-    mint_pubkey: Pubkey,
-    amount: u64,
-    decimals: u8,
+use super::{
+    program_test::{TestContext, TokenContext},
+    token::{ExtensionInitializationParams, Token},
+};
+
+/// Send transaction to validator using `BanksClient::process_transaction`.
+#[derive(Debug, Clone, Copy, Default)]
+pub struct ProgramBanksClientProcessTransaction;
+
+const TEST_MAXIMUM_FEE: u64 = 10_000_000;
+const TEST_FEE_BASIS_POINTS: u16 = 250;
+
+fn test_transfer_fee() -> TransferFee {
+    TransferFee {
+        epoch: 0.into(),
+        transfer_fee_basis_points: TEST_FEE_BASIS_POINTS.into(),
+        maximum_fee: TEST_MAXIMUM_FEE.into(),
+    }
 }
 
-pub async fn create_mint_token22_memo_fee(
-    client: &WasmClient,
-    signer_pubkey: &Pubkey,
-    receiver_pubkey: &Pubkey,
-    mint_pubkey: &Pubkey,
-    amount: u64,
-    decimals: u8,
-) -> anyhow::Result<Transaction> {
-    // 1. create associated if need.
-    let assoc =
-        spl_associated_token_account::get_associated_token_address(&receiver_pubkey, &mint_pubkey);
-
-    let assoc_instruction = create_associated_token_account(
-        signer_pubkey,
-        &receiver_pubkey,
-        &mint_pubkey,
-        &spl_token::ID,
-    );
-
-    // 2. create mint by mint_pubkey
-    let mint_to_instruction = spl_token::instruction::mint_to(
-        &spl_token::ID,
-        &mint_pubkey,
-        &assoc,
-        &signer_pubkey,
-        &[&signer_pubkey],
-        amount,
-    )?;
-
-    // 3. mint to
-    let recent_blockhash = client.get_latest_blockhash().await?;
-    let transaction = Transaction::new_with_payer(
-        &[assoc_instruction, mint_to_instruction],
-        Some(&signer_pubkey),
-    );
-
-    // let result = client.send_and_confirm_transaction(&transaction).await?;
-
-    // println!("SPL Tokens minted successfully.");
-    // println!("Amount: {}", amount);
-    // println!("Receiver pubkey: {}", receiver_pubkey.to_string());
-    // println!("Associated token account: {}", assoc.to_string());
-
-    // Ok(MintToInfo {
-    //     mint_pubkey: *mint_pubkey,
-    //     amount,
-    //     decimals,
-    // })
-    Ok(transaction)
+fn test_transfer_fee_config() -> TransferFeeConfig {
+    let transfer_fee = test_transfer_fee();
+    TransferFeeConfig {
+        transfer_fee_config_authority: COption::Some(Pubkey::new_unique()).try_into().unwrap(),
+        withdraw_withheld_authority: COption::Some(Pubkey::new_unique()).try_into().unwrap(),
+        withheld_amount: 0.into(),
+        older_transfer_fee: transfer_fee,
+        newer_transfer_fee: transfer_fee,
+    }
 }
 
-#[derive(Debug)]
-pub struct NftPayload {
-    pub wallet_address_string: String,
-    pub uri: String,
-    pub title: String,
+struct TransferFeeConfigWithKeypairs {
+    transfer_fee_config: TransferFeeConfig,
+    transfer_fee_config_authority: Keypair,
+    withdraw_withheld_authority: Keypair,
+}
+
+fn test_transfer_fee_config_with_keypairs() -> TransferFeeConfigWithKeypairs {
+    let transfer_fee = test_transfer_fee();
+    let transfer_fee_config_authority = Keypair::new();
+    let withdraw_withheld_authority = Keypair::new();
+    let transfer_fee_config = TransferFeeConfig {
+        transfer_fee_config_authority: COption::Some(transfer_fee_config_authority.pubkey())
+            .try_into()
+            .unwrap(),
+        withdraw_withheld_authority: COption::Some(withdraw_withheld_authority.pubkey())
+            .try_into()
+            .unwrap(),
+        withheld_amount: 0.into(),
+        older_transfer_fee: transfer_fee,
+        newer_transfer_fee: transfer_fee,
+    };
+    TransferFeeConfigWithKeypairs {
+        transfer_fee_config,
+        transfer_fee_config_authority,
+        withdraw_withheld_authority,
+    }
+}
+
+struct TokenWithAccounts {
+    context: TestContext,
+    // token: Token<ProgramBanksClientProcessTransaction>,
+    // token_unchecked: Token<ProgramBanksClientProcessTransaction>,
+    transfer_fee_config: TransferFeeConfig,
+    withdraw_withheld_authority: Keypair,
+    freeze_authority: Keypair,
+    alice: Keypair,
+    alice_account: Pubkey,
+    bob_account: Pubkey,
+}
+
+async fn create_mint_with_accounts(alice_amount: u64) -> TokenWithAccounts {
+    let TransferFeeConfigWithKeypairs {
+        transfer_fee_config_authority,
+        withdraw_withheld_authority,
+        transfer_fee_config,
+        ..
+    } = test_transfer_fee_config_with_keypairs();
+    let mut context = TestContext::new().await;
+    let transfer_fee_basis_points = u16::from(
+        transfer_fee_config
+            .newer_transfer_fee
+            .transfer_fee_basis_points,
+    );
+    let maximum_fee = u64::from(transfer_fee_config.newer_transfer_fee.maximum_fee);
+    context
+        .init_token_with_freezing_mint(vec![ExtensionInitializationParams::TransferFeeConfig {
+            transfer_fee_config_authority: transfer_fee_config_authority.pubkey().into(),
+            withdraw_withheld_authority: withdraw_withheld_authority.pubkey().into(),
+            transfer_fee_basis_points,
+            maximum_fee,
+        }])
+        .await
+        .unwrap();
+    let TokenContext {
+        mint_authority,
+        freeze_authority,
+        token,
+        // token_unchecked,
+        alice,
+        bob,
+        ..
+    } = context.token_context.take().unwrap();
+
+    // token account is self-owned just to test another case
+    token
+        .create_auxiliary_token_account(&alice, &alice.pubkey())
+        .await
+        .unwrap();
+    let alice_account = alice.pubkey();
+    let bob_account = Keypair::new();
+    // token
+    //     .create_auxiliary_token_account(&bob_account, &bob.pubkey())
+    //     .await
+    //     .unwrap();
+    let bob_account = bob_account.pubkey();
+
+    // // mint tokens
+    // token
+    //     .mint_to(
+    //         &alice_account,
+    //         &mint_authority.pubkey(),
+    //         alice_amount,
+    //         &[&mint_authority],
+    //     )
+    //     .await
+    //     .unwrap();
+    TokenWithAccounts {
+        context,
+        // token,
+        // token_unchecked,
+        transfer_fee_config,
+        withdraw_withheld_authority,
+        freeze_authority: freeze_authority.unwrap(),
+        alice,
+        alice_account,
+        bob_account,
+    }
 }
 
 #[cfg(test)]
 mod test {
-    use std::str::FromStr;
-
-    use solana_web3_wasm::{
-        core::client::Web3WasmClient,
-        solana_client_wasm::WasmClient,
-        solana_extra_wasm::program::spl_token,
-        solana_sdk::{
-            pubkey::Pubkey, signature::Keypair, signer::Signer, system_instruction,
-            transaction::Transaction,
-        },
-    };
-
-    use crate::tools::minter::create_mint_token22_memo_fee;
-
-    const AIRDROP_AMOUNT: u64 = 10000000; // tx free of 5000 lamports included
-
-    async fn wait_for_balance_change(
-        client: &WasmClient,
-        account: &Pubkey,
-        balance_before: u64,
-        expected_change: u64,
-    ) {
-        let mut i = 0;
-        let max_loops = 60;
-        loop {
-            let balance_after = client.get_balance(account).await.unwrap();
-            // NOTE might happen that alice is airdropped only after she
-            // transferred the amount to BOB
-            match balance_after.checked_sub(balance_before) {
-                Some(0) => {
-                    std::thread::sleep(std::time::Duration::from_secs(1));
-                    i += 1;
-                    dbg!(i);
-                }
-                Some(delta) => {
-                    assert_eq!(delta, expected_change);
-                    break;
-                }
-                None => {
-                    assert_eq!(balance_before - balance_after, expected_change);
-                    break;
-                }
-            }
-            if i == max_loops {
-                panic!("test was running for {} seconds", max_loops);
-            }
-        }
-    }
+    use solana_web3_wasm::{core::client::Web3WasmClient, solana_client_wasm::WasmClient};
 
     #[tokio::test]
     async fn test_create_mint_token22_memo_fee() {
         let client: WasmClient = Web3WasmClient::new_devnet();
 
-        let alice = Keypair::new();
-
-        let balance_before_airdrop_alice = client.get_balance(&alice.pubkey()).await.unwrap();
-
-        client
-            .request_airdrop(&alice.pubkey(), AIRDROP_AMOUNT)
-            .await
-            .unwrap();
-
-        // Wait for richer Alice
-        wait_for_balance_change(
-            &client,
-            &alice.pubkey(),
-            balance_before_airdrop_alice,
-            AIRDROP_AMOUNT,
-        )
-        .await;
-
-        let balance_after = client.get_balance(&alice.pubkey()).await.unwrap();
-        assert!(balance_after > balance_before_airdrop_alice);
-
-        // Alice is ready
-        let space = 0;
-        let rent_exemption_amount = client
-            .get_minimum_balance_for_rent_exemption(space)
-            .await
-            .unwrap();
-
-        // Bob ready
-        let bob = Keypair::new();
-
-        // Mint address
-        let mint_pubkey = Pubkey::new_unique();
-        let create_mint_token22_memo_fee_tx = create_mint_token22_memo_fee(
-            &client,
-            &alice.pubkey(),
-            &bob.pubkey(),
-            &mint_pubkey,
-            10000000u64,
-            6u8,
-        )
-        .await
-        .unwrap();
-
-        let signature = client
-            .send_transaction(&create_mint_token22_memo_fee_tx)
-            .await;
-        println!("signature{:?}", signature);
-        assert!(signature.is_ok())
+        todo!();
     }
 }
--- worker/web3lite/src/tools/mod.rs
+++ worker/web3lite/src/tools/mod.rs
@@ -1,1 +1,3 @@
 pub mod minter;
+pub mod program_test;
+pub mod token;
