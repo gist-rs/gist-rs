ff512a9cfd168a1591e7540e3b6e07d33c6da136
katopz <katopz@gmail.com>
2023-07-10 11:16:38 +0700

    wip: give up env

--- public/pages/Auth.tsx
+++ public/pages/Auth.tsx
@@ -1,11 +1,8 @@
 import { render } from 'preact'
 import { useState } from 'preact/hooks'
 import { get_user_session } from '../lib/cf'
-import { merge_process_env_to_import_env } from '../utils/env'
 
 const Auth = () => {
-  merge_process_env_to_import_env()
-
   const user_session = get_user_session()
   console.log('user_session:', user_session)
   const [pubkey] = useState(user_session.pubkey)
@@ -30,7 +27,7 @@ const Auth = () => {
         <code>import.meta.env.WMR_GOOGLE_CLIENT_ID:{import.meta.env.WMR_GOOGLE_CLIENT_ID}</code>
       </pre>
       <pre>
-        <code>--import.meta.env.GOOGLE_CLIENT_ID:{import.meta.env.GOOGLE_CLIENT_ID}</code>
+        <code>import.meta.env.GOOGLE_CLIENT_ID:{import.meta.env.GOOGLE_CLIENT_ID}</code>
       </pre>
       <pre>
         <code>process.env.WMR_GOOGLE_CLIENT_ID:{process.env.WMR_GOOGLE_CLIENT_ID}</code>
@@ -39,14 +36,20 @@ const Auth = () => {
         <code>process.env.GOOGLE_CLIENT_ID:{process.env.GOOGLE_CLIENT_ID}</code>
       </pre>
       <pre>
-        <code>{JSON.stringify(import.meta.env, null, 2)}</code>
+        <code>import.meta.env:{JSON.stringify(import.meta.env, null, 2)}</code>
       </pre>
       <pre>
-        <code>{JSON.stringify(process.env, null, 2)}</code>
+        <code>process.env:{JSON.stringify(process.env, null, 2)}</code>
       </pre>
       <script src="https://accounts.google.com/gsi/client" async defer></script>
       <div style={{ width: 'fit-content' }}>
-        <div id="g_id_onload" data-ux_mode="redirect" data-client_id={import.meta.env.GOOGLE_CLIENT_ID} data-callback={import.meta.env.GOOGLE_REDIRECT_URL} data-auto_prompt="false"></div>
+        <div
+          id="g_id_onload"
+          data-ux_mode="redirect"
+          data-client_id={import.meta.env.GOOGLE_CLIENT_ID || process.env.WMR_GOOGLE_CLIENT_ID}
+          data-callback={import.meta.env.GOOGLE_REDIRECT_URL || process.env.WMR_GOOGLE_REDIRECT_URL}
+          data-auto_prompt="false"
+        ></div>
         <div class="g_id_signin" data-type="standard" data-size="large" data-theme="outline" data-text="sign_in_with" data-shape="rectangular" data-logo_alignment="left"></div>
       </div>
       <a href={`/u/${pubkey}`}>accept term and continue</a>
